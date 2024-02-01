2861e35c79af846db11048136b7160ee87ce1808
Nelson KUATE <emmanuel-nelson_kuate-simo@etu.u-bourgogne.fr>
2024-02-01 11:44:17 +0000

    Voici le premier code du controle keyboard qui permet de demarrer et stopper l'enregistrement video de la webcam de mon ordinateur à partir des touches du clavier

---
+++ keyboard.py
@@ -0,0 +1,31 @@
+# Imporation des librairie nécessaire
+import cv2 as cv 
+
+#char c
+
+cap = cv.VideoCapture(0)
+
+def take_off():    # Wake up, cette fonction permet de lancer  la lecture video de la webcam et de resize l'image    
+    while True:
+        ret, frame = cap.read()
+        #img = img.get_frame_read().frame
+        img = cv.flip(frame, 1)
+        #img = cv.resize(img, (780,420))
+        #img, info = findFace(img)
+        #pError = trackFace(info, w, pid, pError)
+        #print("center", info[0], "Area", info[1])
+        cv.imshow("Output", img)
+        if cv.waitKey(1) & 0xFF== ord('q'):
+            break
+
+
+def land():
+    cap.close()
+
+print("Voici notre premier test avec le keyboard et l'enregistrement de video")
+print("En attente de commande !")
+c = input() #attente de la commande par pression d'une touche de clavier
+if (c == 'o'):
+    take_off() # Appel de la focntion take_off si le bouton appuyé est o
+elif (c == 'q'):
+    land() # Appel de la fonction permettant de metrre fin au processus de capture vidéo
