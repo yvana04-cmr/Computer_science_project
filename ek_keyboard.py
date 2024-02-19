a874f751d0f9ac9f2c6eb7272952f3e5460869f2
Nelson KUATE <emmanuel-nelson_kuate-simo@etu.u-bourgogne.fr>
2024-02-19 11:07:53 +0000

    Contrôle du lancement de l'enrregistrement de la webcam avec les touches du clavier

--- ek_keyboard.py
+++ ek_keyboard.py
@@ -1,6 +1,7 @@
 # Imporation des librairie nécessaire
 import cv2 as cv 
 import keyboard as key
+import time
 
 #char c
 
@@ -16,19 +17,24 @@ def take_off():    # Wake up, cette fonction permet de lancer  la lecture video 
         #pError = trackFace(info, w, pid, pError)
         #print("center", info[0], "Area", info[1])
         cv.imshow("Output", img)
-        if cv.waitKey(1) & 0xFF== ord('q'):
-            break
+        if key.is_pressed('q'):
+            land()
 
 
 def land():
-    cap.close()
+    cap.release()
+    cv.destroyAllWindows()
 
 print("Voici notre premier test avec le keyboard et l'enregistrement de video")
 print("En attente de commande !")
  #print(key.is_pressed('ctrl'))
-c = key.record() #attente de la commande par pression d'une touche de clavier
-if (c == 'o'):
-    print('c')
-    take_off() # Appel de la focntion take_off si le bouton appuyé est o
-elif (c == 'q'):
-    land() # Appel de la fonction permettant de metrre fin au processus de capture vidéo
+#c = key.is_pressed() #attente de la commande par pression d'une touche de clavier
+
+while True:
+    if key.is_pressed('t'):
+        take_off()
+        break
+
+
+
+
