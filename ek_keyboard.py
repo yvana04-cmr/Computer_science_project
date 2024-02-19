b576f8d4b97819367aa3bbebcaa20d8b37287f16
Nelson KUATE <emmanuel-nelson_kuate-simo@etu.u-bourgogne.fr>
2024-02-19 07:49:06 +0000

    Importation de la librairie keyboard pour le commande par touche de clavier et test pour lancer la caméra de mon PC!

--- ek_keyboard.py
+++ ek_keyboard.py
@@ -1,5 +1,6 @@
 # Imporation des librairie nécessaire
 import cv2 as cv 
+import keyboard as key
 
 #char c
 
@@ -24,8 +25,10 @@ def land():
 
 print("Voici notre premier test avec le keyboard et l'enregistrement de video")
 print("En attente de commande !")
-c = input() #attente de la commande par pression d'une touche de clavier
+ #print(key.is_pressed('ctrl'))
+c = key.record() #attente de la commande par pression d'une touche de clavier
 if (c == 'o'):
+    print('c')
     take_off() # Appel de la focntion take_off si le bouton appuyé est o
 elif (c == 'q'):
     land() # Appel de la fonction permettant de metrre fin au processus de capture vidéo
